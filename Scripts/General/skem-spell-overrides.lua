51d61c21f96a858b0443f26ecc9d68f1860cf8a1
RawSugr <106305155+RawSugr@users.noreply.github.com>
2023-02-14 09:16:38 +0100

    Update skem-spell-overrides.lua

--- Scripts/General/skem-spell-overrides.lua
+++ Scripts/General/skem-spell-overrides.lua
@@ -842,7 +842,10 @@ local function modifiedMonsterCalculateDamage(d, def, monsterPointer, attackType
 	-- get monster
 
 	local monsterIndex, monster = GetMonster(d.edi)
+local ADAPTIVE = string.lower(SETTINGS["AdaptiveMonsterMode"])
+if ((ADAPTIVE == "default") or (ADAPTIVE == "disabled")) then
 	Mlevel = Game.MonstersTxt[monster.Id].Level
+else Mlevel = monsterArray["Level"]
 	-- execute original code
 
 	local damage = def(monsterPointer, attackType)
